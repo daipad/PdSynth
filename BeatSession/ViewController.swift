2ae08864f80115594204f14636605a9f10e7ebea
daisuke <daiscloud@icloud.com>
2017-02-16 00:02:45 +0900

    commit

--- BeatSession/ViewController.swift
+++ BeatSession/ViewController.swift
@@ -9,15 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBAction func MainMenu(_ segue:UIStoryboardSegue){
+        print("Menu")
+    }
+    
+    
+
 
     override func viewDidLoad() {
+        
+        //NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(ViewController.beatRepeat(_:)), userInfo: nil, repeats: true);
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    //画面の向き
+    override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
+        
+        let orientation: UIInterfaceOrientationMask = UIInterfaceOrientationMask.landscapeRight;
+        
+        return orientation;
+        
+    }
+    override var shouldAutorotate : Bool {
+        return true;
     }
 
 
